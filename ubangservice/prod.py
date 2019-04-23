58db111f437ea59bebf735b7331d361c67fb045a
mars <mars.jinxing@gmail.com>
2019-04-23 12:19:36 +0400

    commit

--- ubangservice/prod.py
+++ ubangservice/prod.py
@@ -6,7 +6,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['ubangservice.com', '*']
 
 # 生产环境开启跨域
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 # 特别说明，下面这个不需要，因为前端是VueJS构建的，它默认使用static作为静态文件入口，我们nginx配置static为入口即可，保持一致，没Django什么事
 STATIC_URL = '/static/'
