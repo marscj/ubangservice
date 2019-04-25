e57a5b3c13cbd1fff0b9c663ec767153e897d60b
mars <mars.jinxing@gmail.com>
2019-04-25 17:48:07 +0400

    commit

--- ubang/user/admin.py
+++ ubang/user/admin.py
@@ -18,6 +18,15 @@ class ImageInline(GenericTabularInline):
     model = Image
     extra = 1
 
+class OrderTimeListFilter(admin.DateFieldListFilter):
+    
+    def queryset(self, request, queryset):
+        try:
+            return queryset.exclude(**self.used_parameters)
+            # return queryset.filter(**self.used_parameters)
+        except (ValueError, ValidationError) as e:
+            raise IncorrectLookupParameters(e)
+
 @admin.register(CustomUser)
 class CustomerUserAdmin(UserAdmin):
     
@@ -51,9 +60,22 @@ class CustomerUserAdmin(UserAdmin):
         }),
     )
 
-    list_display = ('username', 'company', 'full_name', 'is_driver', 'is_tourguide', 'phone', 'email', 'wechart', 'gender', 'is_staff', 'is_superuser')
-    list_filter = ('company', 'gender', 'is_driver', 'is_tourguide', 'is_staff', 'is_active')
-    search_fields = ('username', 'full_name', 'phone', 'email', 'wechart')
+    list_display = (
+        'username', 'company', 'full_name', 'is_driver', 'is_tourguide', 'phone', 'email', 'wechart', 'gender', 'is_staff', 'is_superuser'
+    )
+
+    list_filter = (
+        'company', 'gender', 'is_driver', 'is_tourguide', 'is_staff', 'is_active',
+        ('order__arrival_time', OrderTimeListFilter),
+        ('order__departure_time', OrderTimeListFilter)
+    )
+
+    search_fields = (
+        'username', 'full_name', 'phone', 'email', 'wechart'
+    )
+
     filter_horizontal = ('user_permissions',)
+
     raw_id_fields = ('company',)
+
     list_display_links = list_display
