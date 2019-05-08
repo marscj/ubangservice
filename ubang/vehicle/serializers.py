302ff72d0d7e1b4e328273c12c13ed7c4afa6562
mars <mars.jinxing@gmail.com>
2019-05-08 13:01:14 +0400

    commit

--- ubang/vehicle/serializers.py
+++ ubang/vehicle/serializers.py
@@ -1,9 +1,30 @@
 from rest_framework import serializers
 
-from .models import Vehicle, Model, Brand
+from .models import Vehicle, Model, Brand, ModelPrice
+from ubang.company.serializers import CompanySerializer
+from ubang.user.serializers import UserSerializer
 
+class BrandSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+class ModelSerializer(serializers.ModelSerializer):
+
+    brand = BrandSerializer()
+
+    class Meta:
+        model = Model
+        fields = '__all__'
 
 class VehicleSerializer(serializers.ModelSerializer):
+
+    model = ModelSerializer()
+
+    company = CompanySerializer()
+
+    driver = UserSerializer()
 
     class Meta:
         model = Vehicle
