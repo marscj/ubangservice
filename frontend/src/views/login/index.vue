3170c08ffb3d1db53b9cc47dce656b9ecc74330b
marscj <mjx_cj@163.com>
2019-06-08 00:48:59 +0400

    commit

--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -68,8 +68,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
