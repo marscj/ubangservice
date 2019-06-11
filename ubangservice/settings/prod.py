c46d95eeed4e9c3de91d93b70b1811f8c980cdb9
marscj <mjx_cj@163.com>
2019-06-11 17:00:25 +0400

    commit

---
+++ ubangservice/settings/prod.py
@@ -0,0 +1,41 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+    ('Mars', 'mjx_cj@gmail.com'),
+)
+
+ALLOWED_HOSTS = ['ubangservice.com', '*']
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename':  os.path.join(BASE_DIR, 'debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
