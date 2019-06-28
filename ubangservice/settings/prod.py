06e8916daab1f54009cb6e1313919f4b52cdcbc5
marscj <mjx_cj@163.com>
2019-06-28 10:53:15 +0400

    commit

--- ubangservice/settings/prod.py
+++ ubangservice/settings/prod.py
@@ -2,10 +2,6 @@ from .base import *
 
 DEBUG = False
 
-ADMINS = (
-    ('Mars', 'mjx_cj@gmail.com'),
-)
-
 ALLOWED_HOSTS = ['ubangservice.com', '*']
 
 SESSION_COOKIE_DOMAIN = 'ubangservice.com'
@@ -28,22 +24,3 @@ JWT_AUTH = {
     'JWT_AUTH_COOKIE': 'jwt_auth_token',
     'JWT_GET_USER_SECRET_KEY': 'ubang.user.models.jwt_get_secret_key'
 }
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'file': {
-#             'level': 'DEBUG',
-#             'class': 'logging.FileHandler',
-#             'filename':  os.path.join(BASE_DIR, 'debug.log'),
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['file'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#     },
-# }
